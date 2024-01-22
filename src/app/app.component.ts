51bdf87ee58eb0b1884e3dce8ade3e084c1b078b
Eduardo Oliveira <50887822+Edbr0@users.noreply.github.com>
2024-01-22 16:45:02 -0400

    project created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import data from '../utils/json.json'
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,27 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'italo';
+  counter = 0;
+  developers = data
+  
+
+  increaseCounter(){
+    this.counter++;
+  }
+
+  decreaseCounter(){
+    this.counter--;
+  }
+
+  sendData(developer: any){
+    console.log(developer)
+  }
+
+  removeDeveloperFromList(index: number){
+    this.developers.splice(index, 1)
+  }
 }
+
